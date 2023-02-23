4ffc59eb48fbd06b32548f80319752a83543f001
Riccardo Pucella <rpucella@forrester.com>
2023-02-23 10:23:13 -0500

    Fix killing processes without warning

--- init.el
+++ init.el
@@ -151,9 +151,9 @@
 (setq inhibit-startup-message t)
 (fset 'yes-or-no-p 'y-or-n-p)
 (setq confirm-nonexistent-file-or-buffer nil)
-(setq kill-buffer-query-functions
-      (remq 'process-kill-buffer-query-function
-            kill-buffer-query-functions))
+;(setq kill-buffer-query-functions
+;      (remq 'process-kill-buffer-query-function
+;            kill-buffer-query-functions))
 
 ;; enable line numbers
 (defun display-line-numbers--turn-on ()
