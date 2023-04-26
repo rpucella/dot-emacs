5415009af23cdbfae30022c12d0367dead000717
Riccardo Pucella <riccardo.pucella@gmail.com>
2023-04-25 22:34:24 -0400

    Zweirn: force default names to have an isolatable uuid

--- _init/zweirn.el
+++ _init/zweirn.el
@@ -568,39 +568,44 @@
       (message "Cursor not over a note"))))
 
 (defun zweirn--fresh-name ()
-  (format "Z-%s.%s" (zweirn--random-uuid) zweirn-default-extension))
+  (format "Z-%s-.%s" (zweirn--random-uuid) zweirn-default-extension))
+
+(defun zweirn--manual-random-uuid ()
+  ;; Code here by Christopher Wellons, 2011-11-18.
+  ;; Edited Hideki Saito further to generate all valid variants
+  ;; for "N" in xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx format.
+  (let* ((myStr (md5 (format "%s%s%s%s%s%s%s%s%s%s"
+                             (user-uid)
+                             (emacs-pid)
+                             (system-name)
+                             (user-full-name)
+                             (current-time)
+                             (emacs-uptime)
+                             (garbage-collect)
+                             (buffer-string)
+                             (random)
+                             (recent-keys)))))
+    (format "%s-%s-4%s-%s%s-%s"
+            (substring myStr 0 8)
+            (substring myStr 8 12)
+            (substring myStr 13 16)
+            (format "%x" (+ 8 (random 4)))
+            (substring myStr 17 20)
+            (substring myStr 20 32))))
 
 (defun zweirn--random-uuid ()
   "Returns a UUID - calls “uuidgen” on MacOS, Linux, and PowelShell on Microsoft Windows."
-  (cond
-   ((string-equal system-type "windows-nt")
-    (string-trim (shell-command-to-string rp/powershell-random-uuid)))
-   ((string-equal system-type "darwin") ; Mac
-    (string-trim (shell-command-to-string "uuidgen")))
-   ((string-equal system-type "gnu/linux")
-    (string-trim (shell-command-to-string "uuidgen")))
-   (t
-    ;; Code here by Christopher Wellons, 2011-11-18.
-    ;; Edited Hideki Saito further to generate all valid variants
-    ;; for "N" in xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx format.
-    (let* ((myStr (md5 (format "%s%s%s%s%s%s%s%s%s%s"
-                               (user-uid)
-                               (emacs-pid)
-                               (system-name)
-                               (user-full-name)
-                               (current-time)
-                               (emacs-uptime)
-                               (garbage-collect)
-                               (buffer-string)
-                               (random)
-                               (recent-keys)))))
-      (format "%s-%s-4%s-%s%s-%s"
-              (substring myStr 0 8)
-              (substring myStr 8 12)
-              (substring myStr 13 16)
-              (format "%x" (+ 8 (random 4)))
-              (substring myStr 17 20)
-              (substring myStr 20 32))))))
+  (let* ((uuid
+          (cond
+           ((string-equal system-type "windows-nt")
+            (string-trim (shell-command-to-string rp/powershell-random-uuid)))
+           ((string-equal system-type "darwin") ; Mac
+            (string-trim (shell-command-to-string "uuidgen")))
+           ((string-equal system-type "gnu/linux")
+            (string-trim (shell-command-to-string "uuidgen")))
+           (t (zweirn--manual-random-uuid)))))
+    ;; Remove - and force uppercase.
+    (upcase (replace-regexp-in-string (regexp-quote "-") "" uuid))))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
